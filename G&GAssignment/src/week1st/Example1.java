f4208bd93bc5f31b8d8c1e4d1fc3b30391d3d422
chinm <chinm@DESKTOP-30IO2L5>
2021-01-16 17:00:06 +0900

    21.01.16 1st week assignment

---
+++ G&GAssignment/src/week1st/Example1.java
@@ -0,0 +1,64 @@
+package week1st;
+
+public class Example1 {
+	
+	public static class TextBuffer {
+		private String content;
+		
+		public TextBuffer(String text) {
+			this.content = text;
+		}
+		
+		/*
+		 * 保存されている「content」の中に、「文字」は何回使われているのかを計算する
+		 * @param chr 文字　（大文字、小文字の区別はない）
+		 * @return contentの中に含まれている「文字」の数
+		 */
+		public int countOf(char chr) {
+			int cnt = 0;
+			if(65 <= chr && chr <= 90) {
+				chr = (char) (chr + 32);
+			}
+			int length = content.length();
+			
+			for(int i = 0; i < length; i++) {
+				String checkContent = content.toLowerCase();
+				
+				/*
+				 * メソードを使わない場合
+				 * char check = content.charAt(i);
+				 * if(65 <= check && check <= 90) {
+				 * 		check = (char) (check + 32);
+				 * }
+				 */
+				
+				char check = checkContent.charAt(i);
+				if(chr == check) cnt++;
+			}
+			
+			return cnt;
+		}
+	}
+	
+	public static void main(String[] args) {
+		final String sampleText =
+				"Four score and seven years ago our fathers brought forth " +
+				"on this continent a new nation, conceived in liberty, " +
+				"and dedicated to the proposition that all men are created equal.";
+		
+		final TextBuffer reader = new TextBuffer(sampleText);
+		
+		final int countOfE = reader.countOf('e');
+		final int countOfF = reader.countOf('F');
+		final int countOfComma = reader.countOf(',');
+		final int countOfSpace = reader.countOf(' ');
+		
+		System.out.println(sampleText);
+		System.out.println("----------------");
+		System.out.println("E: " + countOfE);
+		System.out.println("F: " + countOfF);
+		System.out.println("Comma: " + countOfComma);
+		System.out.println("Space: " + countOfSpace);
+	}
+
+}
